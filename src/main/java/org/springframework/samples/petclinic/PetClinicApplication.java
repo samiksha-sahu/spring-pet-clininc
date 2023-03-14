6cdc41add570cf9adb23dc2732bbb777a4000ca6
samiksha-sahu <51385419+samiksha-sahu@users.noreply.github.com>
2023-03-14 09:49:46 +0000

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,7 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
+//import org.springframework.context.annotation.ImportRuntimeHints;
 
 /**
  * PetClinic Spring Boot Application.
@@ -27,7 +27,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
  *
  */
 @SpringBootApplication
-@ImportRuntimeHints(PetClinicRuntimeHints.class)
+//@ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
