9401f960e0859ed7d8f64db45171a17b4a6e118e
samiksha-sahu <51385419+samiksha-sahu@users.noreply.github.com>
2023-03-16 13:01:26 +0000

    Update PetClinicIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
@@ -33,7 +33,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PetClinicIntegrationTests {
 
-	@LocalServerPort
+	//@LocalServerPort
 	int port;
 
 	@Autowired
